20cdd9d36ab5ab0324ed2ef04c6340cb943183f5
Walter Simon <simonwalter986@gmail.com>
2022-06-10 23:46:22 +0300

    commit tree 3fcce79d39e7247f15bd01cd7c2f0b242bded096

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Walter",
+    streetAddress: "Seattle",
+    }
+
+function employees(company) {
+    return employee;
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee.name = 'Sam';
+    newEmployee.streetAddress = '11 Broadway';
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, key, value) {
+    newEmployee.streetAddress = '12 Broadway';
+    return newEmployee;
+}
+
+function deleteFromEmployeeByKey(newEmployee, key) {
+    newEmployee.name = undefined;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee, key) {
+    delete newEmployee.name;
+    return newEmployee;
+}
